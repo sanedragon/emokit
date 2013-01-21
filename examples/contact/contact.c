4d450552627e74b135f8d4333289bfb89447e942
Dag Arneson <sanedragon@gmail.com>
2013-01-20 21:13:42 -0800

    contact example: order display spatially

--- examples/contact/contact.c
+++ examples/contact/contact.c
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 	while (!quit) {
 		if(emokit_read_data(d) > 0) {						
 			c = emokit_get_next_frame(d);			
-			fprintf(stdout,"\033[H\033[2JPress CTRL+C to exit\n\nContact quality:\nF3  %4d\nFC6 %4d\nP7  %4d\nT8  %4d\nF7  %4d\nF8  %4d\nT7  %4d\nP8  %4d\nAF4 %4d\nF4  %4d\nAF3 %4d\nO2  %4d\nO1  %4d\nFC5 %4d",c.cq.F3, c.cq.FC6, c.cq.P7, c.cq.T8,c.cq.F7, c.cq.F8, c.cq.T7, c.cq.P8, c.cq.AF4, c.cq.F4, c.cq.AF3, c.cq.O2, c.cq.O1, c.cq.FC5);
+			fprintf(stdout,"\033[H\033[2JPress CTRL+C to exit\n\nContact quality:\nAF3  %4d\tAF4  %4d\n F3  %4d\t F4  %4d\n F7  %4d\t F8  %4d\nFC5  %4d\tFC6  %4d\n T7  %4d\t T8  %4d\n P7  %4d\t P8  %4d\n O1  %4d\t O2  %4d\n",c.cq.AF3, c.cq.AF4, c.cq.F3, c.cq.F4, c.cq.F7, c.cq.F8, c.cq.FC5, c.cq.FC6, c.cq.T7, c.cq.T8, c.cq.P7, c.cq.P8, c.cq.O1, c.cq.O2, c.cq.FC5);
 			fflush(stdout);
 		} 	
 	}
